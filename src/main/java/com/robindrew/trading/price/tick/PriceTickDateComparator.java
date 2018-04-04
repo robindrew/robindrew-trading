a71eaab92786b04787eb89ed8963d8f470f739bc
Robin Drew <robin.drew@gmail.com>
2018-04-05 00:56:01 +0100

    Fix sorting

--- src/main/java/com/robindrew/trading/price/tick/PriceTickDateComparator.java
+++ src/main/java/com/robindrew/trading/price/tick/PriceTickDateComparator.java
@@ -20,11 +20,9 @@ public class PriceTickDateComparator implements Comparator<IPriceTick> {
 	public int compare(IPriceTick tick1, IPriceTick tick2) {
 		if (tick1.getTimestamp() <= tick2.getTimestamp()) {
 			return ascending ? -1 : 1;
+		} else {
+			return ascending ? 1 : -1;
 		}
-		if (tick1.equals(tick2)) {
-			return 0;
-		}
-		throw new IllegalArgumentException("Non equal, matching ticks, tick1=" + tick1 + ", tick2=" + tick2);
 	}
 
 	public void sort(List<IPriceTick> list) {
