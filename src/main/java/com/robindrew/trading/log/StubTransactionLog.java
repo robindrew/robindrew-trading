674316114841cf4bd241b8cf787da7b389d9657d
Robin Drew <robin.drew@gmail.com>
2018-04-21 23:27:05 +0100

    Stub transaction log

---
+++ src/main/java/com/robindrew/trading/log/StubTransactionLog.java
@@ -0,0 +1,13 @@
+package com.robindrew.trading.log;
+
+public class StubTransactionLog implements ITransactionLog {
+
+	@Override
+	public void log(Object content) {
+	}
+
+	@Override
+	public void log(String header, Object content) {
+	}
+
+}
