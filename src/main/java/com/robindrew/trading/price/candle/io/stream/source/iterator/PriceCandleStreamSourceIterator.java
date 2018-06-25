69e67e06e3212cb78fb54c594a547ae44fdfbe09
Robin Drew <robin.drew@gmail.com>
2018-06-25 23:45:35 +0100

    Improved iterator

--- src/main/java/com/robindrew/trading/price/candle/io/stream/source/iterator/PriceCandleStreamSourceIterator.java
+++ src/main/java/com/robindrew/trading/price/candle/io/stream/source/iterator/PriceCandleStreamSourceIterator.java
@@ -1,23 +1,20 @@
 package com.robindrew.trading.price.candle.io.stream.source.iterator;
 
+import static com.robindrew.common.util.Check.notNull;
+
 import java.util.Iterator;
 
-import com.robindrew.common.util.Java;
 import com.robindrew.trading.price.candle.IPriceCandle;
 import com.robindrew.trading.price.candle.io.stream.source.IPriceCandleStreamSource;
 
 public class PriceCandleStreamSourceIterator implements Iterator<IPriceCandle> {
 
-	private final IPriceCandleStreamSource source;
-
+	private final IPriceCandleStreamSource wrapped;
+	private IPriceCandle nextCandle = null;
 	private boolean finished = false;
-	private IPriceCandle next = null;
 
-	public PriceCandleStreamSourceIterator(IPriceCandleStreamSource source) {
-		if (source == null) {
-			throw new NullPointerException("source");
-		}
-		this.source = source;
+	public PriceCandleStreamSourceIterator(IPriceCandleStreamSource wrapped) {
+		this.wrapped = notNull("wrapped", wrapped);
 	}
 
 	@Override
@@ -25,38 +22,31 @@ public class PriceCandleStreamSourceIterator implements Iterator<IPriceCandle> {
 		if (finished) {
 			return false;
 		}
-		if (next == null) {
 
-			try {
-				next = source.getNextCandle();
-			} catch (Exception e) {
-				handleException(e);
-				next = null;
-			}
-
-			if (next == null) {
-				finished = true;
-				return false;
-			}
+		// Already have next candle ready?
+		if (nextCandle != null) {
+			return true;
 		}
-		return true;
-	}
 
-	protected void handleException(Exception e) {
-		throw Java.propagate(e);
+		// Prepare next candle
+		nextCandle = wrapped.getNextCandle();
+		if (nextCandle != null) {
+			return true;
+		}
+
+		// No more candles, finished.
+		finished = true;
+		return false;
 	}
 
 	@Override
 	public IPriceCandle next() {
-		if (finished) {
-			throw new IllegalStateException("Do not call next() if hasNext() returns false, no more candles");
+		if (!hasNext()) {
+			throw new IllegalStateException("No more candles");
 		}
-		if (next == null) {
-			throw new IllegalStateException("Do not call next() before hasNext()");
-		}
-		IPriceCandle candle = next;
-		next = null;
-		return candle;
+		IPriceCandle next = nextCandle;
+		nextCandle = null;
+		return next;
 	}
 
 }
